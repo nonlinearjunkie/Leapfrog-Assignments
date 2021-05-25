44649258a61e1a8adaf148b99f016f911f0e5fe0
Suraj Joshi <surajjoshi0123@gmail.com>
2021-05-25 17:04:29 +0545

    Remove unwanted Code

--- Equations-To-Plot/js/FunctionTypes/gaussianFunction.js
+++ Equations-To-Plot/js/FunctionTypes/gaussianFunction.js
@@ -228,43 +228,4 @@ class GaussianFunctions extends Function {
 
     this.plotPoints();
   }
-
-  // renderPlot() {
-  //   //Negative value of standard deviation is not practical
-  //   if (this.standardDeviation > 0) {
-  //     console.log("Plot of Gaussian function");
-
-  //     this.renderPlotOutline();
-
-  //     var canvas = document.querySelector("canvas");
-  //     var cntxt = canvas.getContext("2d");
-  //     cntxt.beginPath();
-
-  //     for (let i = -(canvas.width / 10); i < canvas.width / 10; i += 0.1) {
-  //       let x_cord = i;
-  //       //Scale the points
-  //       let xCordScaled = x_cord * 5;
-  //       //Translate the points so that (0,0) lies at mid bottom
-  //       let xCordTranslated = canvas.width / 2 + xCordScaled;
-
-  //       let variance = this.standardDeviation ** 2;
-
-  //       let amplitude = 1 / Math.sqrt(2 * Math.PI * variance); // (1/sigma *(sqrt(2*pi)))
-
-  //       let exponentNumerator = -((i - this.mean) ** 2);
-  //       let expoentDenominator = 2 * variance;
-  //       let exponent = exponentNumerator / expoentDenominator;
-  //       let y_cord = amplitude * Math.E ** exponent;
-
-  //       let yCordScaled = y_cord * 1000; //Multiply heavily as the results are probabilities are very low
-  //       let yCordTranslated = canvas.height - 50 - yCordScaled;
-  //       cntxt.lineTo(xCordTranslated, yCordTranslated);
-  //     }
-
-  //     cntxt.strokeStyle = "black";
-  //     cntxt.stroke();
-  //   } else {
-  //     console.log("Standard Deviation cannot be negative");
-  //   }
-  // }
 }
