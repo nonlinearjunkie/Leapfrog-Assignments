cca08a513c748b42f634f3f1f86c94fc8582e95f
Suraj Joshi <surajjoshi0123@gmail.com>
2021-05-25 17:05:12 +0545

    Add Slider to logarithm

--- Equations-To-Plot/js/FunctionTypes/logarithmicFunctions.js
+++ Equations-To-Plot/js/FunctionTypes/logarithmicFunctions.js
@@ -16,8 +16,8 @@ class LogarithmicFunctions extends Function {
     console.log(canvas);
     var cntxt = canvas.getContext("2d");
     cntxt.beginPath();
-    cntxt.moveTo(0, canvas.height - 50);
-    cntxt.lineTo(canvas.width, canvas.height - 50);
+    cntxt.moveTo(0, canvas.height / 2);
+    cntxt.lineTo(canvas.width, canvas.height / 2);
     cntxt.strokeStyle = "#FF0000";
     cntxt.stroke();
   }
@@ -60,35 +60,128 @@ class LogarithmicFunctions extends Function {
     this.drawYAxis();
   }
 
-  renderPlot() {
-    //The base of logarithm must be greater than 0
-    if (this.base > 0) {
-      console.log("Plot of Logarithmic function");
+  showSliders(sliderDivArray) {
+    let mainContainer = document.getElementById("container");
+    let sliderContainer = document.createElement("div");
+    sliderContainer.setAttribute("class", "slider-container left");
 
-      this.renderPlotOutline();
+    let sliderHeading = document.createElement("div");
+    sliderHeading.setAttribute("class", "sliderHeading");
+    sliderHeading.innerHTML = `<h3> Slider Controls</h3>`;
+    sliderContainer.appendChild(sliderHeading);
 
-      var canvas = document.querySelector("canvas");
-      var cntxt = canvas.getContext("2d");
-      cntxt.beginPath();
+    sliderDivArray.forEach(function (sliderDiv) {
+      sliderContainer.appendChild(sliderDiv);
+    });
 
-      for (let i = 0; i < canvas.width; i += 0.1) {
-        let x_cord = i;
-        //Scale the points
-        let xCordScaled = x_cord * 5;
-        //Translate the points so that (0,0) lies at bottom left end
-        let xCordTranslated = 50 + xCordScaled;
+    mainContainer.appendChild(sliderContainer);
+  }
 
-        let y_cord = this.getBaseLog(this.base, i);
+  //Method to create Info about SLider-name and value
+  //Takes name for slider and associated property(Value of object property associated with slider)
 
-        let yCordScaled = y_cord * 10;
-        let yCordTranslated = canvas.height - 50 - yCordScaled;
-        cntxt.lineTo(xCordTranslated, yCordTranslated);
-      }
+  createSliderInfo(sliderName, associatedProperty) {
+    let sliderDivInfo = document.createElement("div"); //container for slider heading and value
+    sliderDivInfo.setAttribute("class", "slider-div-info clearfix");
 
-      cntxt.strokeStyle = "black";
-      cntxt.stroke();
-    } else {
-      console.log("Logarithm exists only for positive bases");
+    let sliderDivHeading = document.createElement("div");
+    sliderDivHeading.innerHTML = `<h4>${sliderName}</h4>`;
+    sliderDivHeading.setAttribute("class", "slider-div-heading left");
+    sliderDivInfo.appendChild(sliderDivHeading);
+
+    let silderDivValue = document.createElement("div");
+    silderDivValue.innerHTML = `<p> Value: ${associatedProperty}`;
+    silderDivValue.setAttribute("class", "slider-div-value left");
+    sliderDivInfo.appendChild(silderDivValue);
+
+    return sliderDivInfo;
+  }
+
+  //method to create slider Input
+  createSliderElement(id, minValue, maxValue, stepSize, associatedProperty) {
+    let sliderInput = document.createElement("input");
+    sliderInput.setAttribute("class", "slider");
+    sliderInput.setAttribute("type", "range");
+    sliderInput.setAttribute("id", id);
+    sliderInput.setAttribute("min", minValue);
+    sliderInput.setAttribute("max", maxValue);
+    sliderInput.setAttribute("step", stepSize);
+    sliderInput.setAttribute("value", associatedProperty);
+
+    return sliderInput;
+  }
+
+  createBaseValueSliderDiv(
+    minValue,
+    maxValue,
+    stepSize,
+    associatedProperty,
+    id,
+    sliderName
+  ) {
+    let self = this;
+
+    let sliderDiv = document.createElement("div");
+    sliderDiv.setAttribute("class", "slider-div");
+
+    let sliderDivInfo = this.createSliderInfo(sliderName, associatedProperty);
+    sliderDiv.appendChild(sliderDivInfo);
+
+    let sliderInput = this.createSliderElement(
+      id,
+      minValue,
+      maxValue,
+      stepSize,
+      associatedProperty
+    );
+    sliderDiv.appendChild(sliderInput);
+    sliderInput.onchange = function () {
+      self.base = this.value;
+      self.renderPlot();
+    };
+
+    return sliderDiv;
+  }
+
+  plotPoints() {
+    var canvas = document.querySelector("canvas");
+    var cntxt = canvas.getContext("2d");
+    cntxt.beginPath();
+
+    for (let i = 0; i < canvas.width; i += 0.1) {
+      let x_cord = i;
+      //Scale the points
+      let xCordScaled = x_cord * 5;
+      //Translate the points so that (0,0) lies at bottom left end
+      let xCordTranslated = 50 + xCordScaled;
+
+      let y_cord = this.getBaseLog(this.base, i);
+
+      let yCordScaled = y_cord * 10;
+      let yCordTranslated = canvas.height / 2 - yCordScaled;
+      cntxt.lineTo(xCordTranslated, yCordTranslated);
     }
+
+    cntxt.strokeStyle = "black";
+    cntxt.stroke();
+  }
+
+  renderPlot() {
+    this.renderPlotOutline();
+
+    let baseValueSliderDiv = this.createBaseValueSliderDiv(
+      0.1,
+      5,
+      0.1,
+      this.base,
+      "logarithm-base",
+      "Set Logarithm Base"
+    );
+
+    let sliderDivArray = [baseValueSliderDiv];
+
+    this.showSliders(sliderDivArray);
+
+    this.plotPoints();
   }
 }
